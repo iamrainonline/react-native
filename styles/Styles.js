0989c5ace2808656e3fdad3035f637ab43c21a56
Cristian <cristian.candidatu@itom.ro>
2024-01-26 15:20:51 +0200

    navigation dependendies + tabs

---
+++ styles/Styles.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+const global = {
+   primary: "white",
+   background: "#000000",
+   accent: "#FFC104",
+   danger: "#EF5858",
+};
+
+export default styles = StyleSheet.create({
+   container: {
+      flex: 1,
+      alignItems: "center",
+      marginTop: 50,
+   },
+});
